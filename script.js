411dea6c69e3896307548965939b919604a01598
ranabir <mail2ranabirghosh@gmail.com>
2018-11-30 00:23:50 +0530

    My Jscript

---
+++ script.js
@@ -0,0 +1,95 @@
+// *******************************
+// START HERE IF YOU WANT A MORE CHALLENGING STARTING POINT FOR THIS ASSIGNMENT
+// *******************************
+//
+// Module 4 Assignment Instructions.
+//
+// The idea of this assignment is to take an existing array of names
+// and then output either Hello 'Name' or Good Bye 'Name' to the console.
+// The program should say "Hello" to any name except names that start with a "J"
+// or "j", otherwise, the program should say "Good Bye". So, the final output
+// on the console should look like this:
+/*
+Hello Yaakov
+Good Bye John
+Good Bye Jen
+Good Bye Jason
+Hello Paul
+Hello Frank
+Hello Larry
+Hello Paula
+Hello Laura
+Good Bye Jim
+
+WARNING!!! WARNING!!!
+The code does NOT currently work! It is YOUR job to make it work
+as described in the requirements and the steps in order to complete this
+assignment.
+WARNING!!! WARNING!!!
+
+*/
+
+// STEP 1:
+// Wrap the entire contents of script.js inside of an IIFE
+// See Lecture 52, part 2
+// (Note, Step 2 will be done in the SpeakHello.js file.)
+/*(function(window){
+  var greeter = {};
+  var greetingHello = "Hello ";
+  var greetingGB = "Good Bye ";
+  greeter.name = "";
+  greeter.greet = function () {
+    if (greeter.name.substring(0,1).toLowerCase()=="j") {console.log(greetingGB + greeter.name)}
+      else console.log(greetingHello + greeter.name);
+  }
+  window.greeter = greeter
+})(window);*/
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+/*for(i in names){
+greeter.name = names[i];
+//console.log(greeter.name);
+greeter.greet();
+}*/
+
+for(i in names){
+  if (names[i].substring(0,1).toLowerCase()=="j"){
+      GoodbyeGreeter.name = names[i];
+      //console.log(HelloGreeter.name);
+      GoodbyeGreeter.speak();
+    }
+  else
+      {HelloGreeter.name = names[i];
+      //console.log(GoodbyeGreeter.name);
+      HelloGreeter.speak();
+    }
+}
+
+// STEP 10:
+// Loop over the names array and say either 'Hello' or "Good Bye"
+// using the 'speak' method or either helloSpeaker's or byeSpeaker's
+// 'speak' method.
+// See Lecture 50, part 1
+//for (/* fill in parts of the 'for' loop to loop over names array */) {
+
+  // STEP 11:
+  // Retrieve the first letter of the current name in the loop.
+  // Use the string object's 'charAt' function. Since we are looking for
+  // names that start with either upper case or lower case 'J'/'j', call
+  // string object's 'toLowerCase' method on the result so we can compare
+  // to lower case character 'j' afterwards.
+  // Look up these methods on Mozilla Developer Network web site if needed.
+  // var firstLetter =
+
+  // STEP 12:
+  // Compare the 'firstLetter' retrieved in STEP 11 to lower case
+  // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
+  // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
+  // name in the loop.
+  //if (/* fill in condition here */) {
+    // byeSpeaker.xxxx
+ // } else {
+    // helloSpeaker.xxxx
+ // }
+//}
